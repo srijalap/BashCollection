20663d626993ec95ca2d3a592efcc71544088a23
Srijana Poudel <srijana.poudel@tuni.fi>
2023-05-03 20:25:29 +0300

    added readme.md file to scripts

--- scripts/tip_of_the_day.sh
+++ scripts/tip_of_the_day.sh
@@ -2,13 +2,6 @@
 
 # Tip of the Day
 
-#tips=(
- # "Tip 1: Keep your server updated with the latest security patches."
-  #"Tip 2: Regularly back up your important data to prevent data loss."
-  #"Tip 3: Use strong and unique passwords for your user accounts."
-  # Add more tips here
-#)
-
 tips_file="$(dirname "$0")/tips.txt"
 
 readarray -t tips < "$tips_file"
