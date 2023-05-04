510b08119e5a016e0e791987c463125ff15340d4
Srijana Poudel <srijana.poudel@tuni.fi>
2023-05-04 15:25:15 +0300

    correction on ex5

--- ex-5-fetch-file.sh
+++ ex-5-fetch-file.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-filename='https://www.rfc-editor.org/rfc-5.txt'
-n=1
-while read line; do
+
+n=$1
+
+## while read n; do
+
+# generate random numbers
+randomNum=$(gshuf -i 1-8000 -n 1)
+
+filename='https://www.rfc-editor.org/rfc/rfc$n.txt'
+wget -q filename -O www.tmp
+
+lines=$(wc -l www.tmp | awk '{print $1}')
+
     # reading each line
-    echo "Line No. $n : $line"
-    n=$((n+1))
-done < $filename
+    echo "Line No. $n : $lines"
